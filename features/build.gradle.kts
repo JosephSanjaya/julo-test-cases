b3304e66c32fd97f0c45d3e9666c28cee3c71507
Joseph Sanjaya <sanjayajosep@gmail.com>
2023-03-13 20:55:15 +0700

    [joseph] feat: init

---
+++ features/build.gradle.kts
@@ -0,0 +1,22 @@
+import com.android.build.gradle.BaseExtension
+
+plugins {
+    id("java-platform")
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+
+    plugins.withType(BasePlugin::class.java).configureEach {
+        configure<BaseExtension> {
+            defaultConfig {
+                consumerProguardFiles("consumer-rules.pro")
+            }
+        }
+    }
+}
+
+dependencies {
+    constraints {
+    }
+}
